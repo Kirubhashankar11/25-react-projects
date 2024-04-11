fa56057c2c55155371c8d24d8949f5807a5d6cd5
kirubhashankar11~ <kirubhashankar1108@gmail.com>
2024-04-11 11:48:52 +0530

    bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
-
+import ImageSlider from './components/image-slider';
 
 function App() {
   return (
@@ -14,7 +14,13 @@ function App() {
       {/* <RandomColor/> */}
 
       {/* star rating components */}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Image Sliderr Component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"} />
     </div>
   );
 }
